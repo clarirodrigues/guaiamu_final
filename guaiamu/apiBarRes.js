585a1ad7a8ddc27cd72f6d7c868575624a70ecd8
Clarice Rodrigues <144385252+clarirodrigues@users.noreply.github.com>
2024-04-11 22:25:40 -0300

    Ultima atualização

---
+++ guaiamu/apiBarRes.js
@@ -0,0 +1,10 @@
+const teste = document.querySelector('#teste')
+
+fetch('./bareserestaurantes.json')
+.then((res) => res.json())
+.then((data) => {
+    data.records.forEach(element => {
+        teste.src += `<a href='https://www.google.com'>${element[1]}</a>`
+        teste.src += `<a href='https://www.google.com'>${element[2]}</a>`
+    });
+})
